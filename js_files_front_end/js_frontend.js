cc9f75d575114bf44ec7b54daf4420f38238b923
fallou <mouhamadou-falilou.kandji@etu.univ-amu.fr>
2022-05-01 06:07:15 +0200

    Add contain to js_frontend.js file

---
+++ js_files_front_end/js_frontend.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const id_Probabilite   = document.getElementById("#id_Probabilité");
+const id_Programmation2 = document.getElementById("#id_Programmation 2");
+const ProC             = document.getElementById("#id_Programmation C et Système");
+const structure        = document.getElementById("#id_Structures discrètes");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    id_Probabilite.innerHTML = "Probabilité";
+    id_Programmation2.innerHTML = "Programmation 2";
+    ProC.innerHTML = "Programmation C et Système";
+    structure.innerHTML = "Structures discrètes"
+})
